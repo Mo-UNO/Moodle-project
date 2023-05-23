45859070ef90b43b6b95eb1ff041d295e887b162
0xzer0x <32334265+0xzer0x@users.noreply.github.com>
2023-05-23 23:17:34 +0300

    partial main.cpp update - Mohammad Haitham

--- main.cpp
+++ main.cpp
@@ -4,8 +4,40 @@ using namespace std;
 
 int main()
 {
-    CPG_Student s1;
-    s1.registerStudent();
-    s1.calculateGpa();
-    s1.getStudentInfo();
+    int n_students, n_courses;
+    cout << "Enter the number of students: ";
+    cin >> n_students;
+    cout << "Enter the number of courses: ";
+    cin >> n_courses;
+    CStudent students[n_students];
+    // CCourse courses[n_courses];
+
+    // Register students
+    for (int i = 0; i < n_students; i++)
+    {
+        cout << "############### Registering Student " << i + 1 << " ###############" << endl;
+        fflush(stdin);
+        students[i].registerStudent();
+    }
+
+    // Register courses
+    for (int i = 0; i < n_courses; i++)
+    {
+        cout << "############### Registering Course " << i + 1 << " ###############" << endl;
+        fflush(stdin);
+    }
+
+    // Display student information
+    cout << "############################### Student Information ###############################" << endl;
+    for (int i = 0; i < n_students; i++)
+    {
+        students[i].getStudentInfo();
+    }
+
+    // Display course information
+    cout << "=========================== Course Information ===========================" << endl;
+    for (int i = 0; i < n_courses; i++)
+    {
+        cout << "Course #" << (i + 1) << endl;
+    }
 }
